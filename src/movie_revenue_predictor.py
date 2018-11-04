9a1c6227e01124bf441db269ac573933c376f0d9
Sayter99 <ing5698yi@hotmail.com>
2018-11-03 20:40:12 -0600

    get # of instagram hashtags

--- src/movie_revenue_predictor.py
+++ src/movie_revenue_predictor.py
@@ -4,26 +4,37 @@ import numpy as np
 import preprocessing
 import sys
 from utilities.instagram_data import AcquireJson
-#import utilities.instagram_data as instagram
+from pathlib import Path
 
-creditsData = pd.read_csv('datasets/tmdb_5000_credits.csv')
-moviesData = pd.read_csv('datasets/tmdb_5000_movies.csv')
-mergedData = preprocessing.mergeDataFrames(creditsData, moviesData)
-# cast -> main actor/actress
-mergedData['cast'] = mergedData['cast'].apply(lambda x: preprocessing.cast2main(x))
-# crew -> director
-mergedData['crew'] = mergedData['crew'].apply(lambda x: preprocessing.crew2director(x))
-# get # of hashtags
 a = AcquireJson()
-mergedData['actor1_hashtags'] = mergedData['cast'].apply(lambda x: a.getHashtagCounts(x.split(',')[0]))
-a.clearCache()
-mergedData['actor2_hashtags'] = mergedData['cast'].apply(lambda x: a.getHashtagCounts(x.split(',')[1]))
-a.clearCache()
-mergedData['actor3_hashtags'] = mergedData['cast'].apply(lambda x: a.getHashtagCounts(x.split(',')[2]))
-a.clearCache()
-mergedData['director_hashtags'] = mergedData['crew'].apply(lambda x: a.getHashtagCounts(x))
-a.clearCache()
-mergedData['movie_hashtags'] = mergedData['title_x'].apply(lambda x: a.getHashtagCounts(x))
-mergedData.to_csv('datasets/merged.csv')
+integratedCSV = Path('datasets/integrated.csv')
+integratedRefilledCSV = Path('datasets/integratedRefilled.csv')
+if not integratedCSV.exists():
+    creditsData = pd.read_csv('datasets/tmdb_5000_credits.csv')
+    moviesData = pd.read_csv('datasets/tmdb_5000_movies.csv')
+    mergedData = preprocessing.mergeDataFrames(creditsData, moviesData)
+    # cast -> main actor/actress
+    mergedData['cast'] = mergedData['cast'].apply(lambda x: preprocessing.cast2main(x))
+    # crew -> director
+    mergedData['crew'] = mergedData['crew'].apply(lambda x: preprocessing.crew2director(x))
+    # get # of hashtags
+    mergedData['actor1_hashtags'] = mergedData['cast'].apply(lambda x: a.getHashtagCounts(x.split(',')[0]))
+    a.clearCache()
+    mergedData['actor2_hashtags'] = mergedData['cast'].apply(lambda x: a.getHashtagCounts(x.split(',')[1]))
+    a.clearCache()
+    mergedData['actor3_hashtags'] = mergedData['cast'].apply(lambda x: a.getHashtagCounts(x.split(',')[2]))
+    a.clearCache()
+    mergedData['director_hashtags'] = mergedData['crew'].apply(lambda x: a.getHashtagCounts(x))
+    a.clearCache()
+    mergedData['movie_hashtags'] = mergedData['title_x'].apply(lambda x: a.getHashtagCounts(x))
+    mergedData.to_csv('datasets/integrated.csv')
+elif not integratedRefilledCSV.exists():
+    mergedData = pd.read_csv('datasets/integrated.csv')
+    mergedData['crew'] = mergedData['crew'].replace(np.nan, 'N/A')
+    mergedData.to_csv('datasets/test.csv')
+    mergedData.apply(lambda x: a.refillMissingTags(x), axis = 1)
+    mergedData.to_csv('datasets/integratedRefilled.csv')
+else:
+    pass
 # deal with missing values
 #####mergedData = preprocessing.dropMissingValues(mergedData)
--- src/utilities/instagram_data.py
+++ src/utilities/instagram_data.py
@@ -19,8 +19,8 @@ class AcquireJson():
             return None
         return data
 
-    def getHashtagCounts(self, tag):
-        retry = 5
+    def str2InstagramStyledTag(self, tag):
+        tag = str(tag)
         working_tag = tag.replace(' ', '')
         working_tag = working_tag.replace('.', '')
         working_tag = working_tag.replace('-', '')
@@ -29,12 +29,40 @@ class AcquireJson():
         working_tag = working_tag.replace(',', '')
         working_tag = working_tag.replace('·', '')
         working_tag = working_tag.replace('&', '')
+        working_tag = working_tag.replace('/', '')
+        working_tag = working_tag.replace('!', '')
+        working_tag = working_tag.replace('?', '')
+        working_tag = working_tag.replace('(', '')
+        working_tag = working_tag.replace(')', '')
+        working_tag = working_tag.replace('_', '')
+        working_tag = working_tag.replace('@', '')
+        working_tag = working_tag.replace('{', '')
+        working_tag = working_tag.replace('}', '')
+        working_tag = working_tag.replace('#', '')
+        working_tag = working_tag.replace('$', '')
+        working_tag = working_tag.replace('%', '')
+        working_tag = working_tag.replace('^', '')
+        working_tag = working_tag.replace('+', '')
+        working_tag = working_tag.replace('\\', '')
+        working_tag = working_tag.replace('\"', '')
+        working_tag = working_tag.replace('[', '')
+        working_tag = working_tag.replace(']', '')
+        working_tag = working_tag.replace('~', '')
+        working_tag = working_tag.replace('’', '')
+        working_tag = working_tag.replace('°', '')
+        return working_tag
+
+    def getHashtagCounts(self, tag):
+        if str(tag) == 'N/A':
+            return 0
+        if str(tag) == '' or str(tag) == ' ':
+            return 0
+        retry = 5
+        working_tag = self.str2InstagramStyledTag(tag)
         tags = self.acquired.get(working_tag, 0)
         if tags != 0:
             print ('INFO: Tags of ' + working_tag + ' = ' + str(tags))
             return tags
-        if working_tag == 'N/A':
-            return 0
         print('INFO: Getting tag counts of ' + working_tag)
         url = 'https://www.instagram.com/explore/tags/' + urllib.parse.quote_plus(working_tag) + '?__a=1'
         for i in range(retry):
@@ -53,6 +81,24 @@ class AcquireJson():
         print ('INFO: Tags of ' + working_tag + ' = ' + str(tags))
         self.acquired[working_tag] = tags
         return tags
+    
+    def refillMissingTags(self, data):
+        if (data['actor1_hashtags'] == 0):
+            if (data.cast.split(',')[0].replace(' ', '') != 'N/A'):
+                data.actor1_hashtags = self.getHashtagCounts(data.cast.split(',')[0].replace(' ', ''))
+        if (data['actor2_hashtags'] == 0):
+            if (data.cast.split(',')[1].replace(' ', '') != 'N/A'):
+                data.actor2_hashtags = self.getHashtagCounts(data.cast.split(',')[1].replace(' ', ''))
+        if (data['actor3_hashtags'] == 0):
+            if (data.cast.split(',')[2].replace(' ', '') != 'N/A'):
+                data.actor3_hashtags = self.getHashtagCounts(data.cast.split(',')[2].replace(' ', ''))
+        if (data['director_hashtags'] == 0):
+            if (data.crew.replace(' ', '') != 'N/A'):
+                data.director_hashtags = self.getHashtagCounts(data.crew.replace(' ', ''))
+        if (data['movie_hashtags'] == 0):
+            if (data.title_x.replace(' ', '') != 'N/A'):
+                data.movie_hashtags = self.getHashtagCounts(data.title_x.replace(' ', ''))
+        pass
 
     def clearCache(self):
         self.acquired.clear()
